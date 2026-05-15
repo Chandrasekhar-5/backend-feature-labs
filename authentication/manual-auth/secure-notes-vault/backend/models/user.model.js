5dd48db6bd7e663dfafc98a323c790fbe4dac0d2
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-05-15 23:09:07 +0530

    user model

---
+++ authentication/manual-auth/secure-notes-vault/backend/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: [true, 'Username already exists']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: [true, 'Email already exists']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+}, { timestamps: true });
+
+
+const userModel = mongoose.model('User', userSchema);
+
+export default userModel;
