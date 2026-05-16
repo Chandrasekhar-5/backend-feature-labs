d63e86c74a10ba1f78905957f08f58ebbfdab9f3
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-05-16 14:06:16 +0530

    implemented login logic, created login route, and used that route

--- authentication/manual-auth/secure-notes-vault/backend/controllers/auth.controller.js
+++ authentication/manual-auth/secure-notes-vault/backend/controllers/auth.controller.js
@@ -85,3 +85,80 @@ export async function register(req, res) {
         res.status(500).json({ message: error.message });
     }
 }
+
+
+export async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+
+        const user = await userModel.findOne({ email });
+
+
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+
+        const refreshToken = jwt.sign(
+            { id: user._id }, 
+            config.JWT_SECRET, 
+            { expiresIn: '7d' }
+        );
+
+
+        const refreshTokenHash = await bcrypt.hash(refreshToken, 10);
+
+
+        const session = await sessionModel.create({
+            user: user._id,
+            refreshTokenHash,
+            ip: req.ip,
+            userAgent: req.headers['user-agent']
+        });
+
+
+        const accessToken = jwt.sign(
+            { user: user._id, session: session._id },
+            config.JWT_SECRET,
+            { expiresIn: '15m' }
+        );
+
+
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'strict',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+
+
+        res.status(200).json({
+            message: 'User logged in successfully',
+            user: {
+                username: user.username,
+                email: user.email,
+                role: user.role
+            },
+            accessToken
+        });
+
+
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- authentication/manual-auth/secure-notes-vault/backend/routes/auth.route.js
+++ authentication/manual-auth/secure-notes-vault/backend/routes/auth.route.js
@@ -7,4 +7,7 @@ const router = Router();
 router.post('/register', authController.register);
 
 
+router.get('/login', authController.login);
+
+
 export default router;
