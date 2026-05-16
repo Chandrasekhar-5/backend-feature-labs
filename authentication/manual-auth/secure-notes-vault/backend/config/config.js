2ca0ee9ff3b15a9d695b4daf0d33875669c2b2f3
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-05-16 12:15:01 +0530

    jwt secret config, cookie parser use, and new packages

--- authentication/manual-auth/secure-notes-vault/backend/config/config.js
+++ authentication/manual-auth/secure-notes-vault/backend/config/config.js
@@ -10,9 +10,14 @@ if (!process.env.MONGO_URI) {
     throw new Error('Missing MONGO_URI environment variable');
 }
 
+if (!process.env.JWT_SECRET) {
+    throw new Error('Missing JWT_SECRET environment variable');
+}
+
 const config = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET
 }
 
 export default config;
--- authentication/manual-auth/secure-notes-vault/backend/src/app.js
+++ authentication/manual-auth/secure-notes-vault/backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 
 
