1026fb30408bc9db053e4622d268d0c72676f9f7
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-05-17 21:22:41 +0530

    implemented get my notes, update note and delete note logics

--- authentication/manual-auth/secure-notes-vault/backend/controllers/note.controller.js
+++ authentication/manual-auth/secure-notes-vault/backend/controllers/note.controller.js
@@ -21,3 +21,69 @@ export async function createNote(req, res) {
         res.status(500).json({ message: error.message });
     }
 }
+
+
+
+export async function getMyNotes(req, res) {
+    try {
+        const notes = await noteModel.find({ owner: req.user._id });
+
+        res.status(200).json({ message: 'All notes fetched successfully', notes });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+export async function updateNote(req, res) {
+    try {
+        const { noteId } = req.params;
+
+        const allowedFields = ['title', 'content'];
+
+        const updates = {};
+
+        Object.entries(req.body).forEach(([key, value]) => {
+            if (allowedFields.includes(key) && value !== undefined) {
+                updates[key] = value;
+            }
+        });
+
+        const note = await noteModel.findByIdAndUpdate(
+            noteId,
+            { $set: updates },
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!note) {
+            return res.status(404).json({ message: 'Note not found' });
+        }
+
+        res.status(200).json({ message: 'Note updated successfully', note });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+export async function deleteNote(req, res) {
+    try {
+        const { noteId } = req.params;
+
+        const note = await noteModel.findByIdAndDelete(noteId);
+
+        if (!note) {
+            return res.status(404).json({ message: 'Note not found' });
+        }
+
+        res.status(200).json({ message: 'Note deleted successfully' });
+    }
+    catch (error){
+        res.status(500).json({ message: error.message });
+    }
+}
--- authentication/manual-auth/secure-notes-vault/backend/routes/note.route.js
+++ authentication/manual-auth/secure-notes-vault/backend/routes/note.route.js
@@ -7,5 +7,11 @@ const router = Router();
 
 router.post('/create-note', protect, noteController.createNote);
 
+router.get('/my-notes', protect, noteController.getMyNotes);
+
+router.put('/update-note/:noteId', protect, noteController.updateNote);
+
+router.delete('/delete-note/:noteId', protect, noteController.deleteNote);
+
 
 export default router;
